66943e1eb95d1107aba8beb5c3384a7ae93efc09
kinandani <156874303+kinandani@users.noreply.github.com>
2024-02-21 13:43:17 +0700

    up2

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const nama = prompt('masukkan nama : ')
 const umur = prompt('masukkan umur : ')
 
 if (umur <= 13) {
-    alert(`Maaf, Anda belum cukup umur`)
+    alert(`Maaf, umur 13 tahun kebawah \n belum bisa masuk studio.`)
 }
 else {
-    const studio = prompt('Pilih studio A/B/C \N\nISI DENGAN HURUF KAPITAL!')
+    const studio = prompt('Pilih studio A/B/C \n \nISI DENGAN HURUF KAPITAL!')
     if(studio == `A`){
-    alert(`Tiket ${nama}, Studio A, Umur ${umur}`)
+    alert(`Tiket ${nama}, Studio A, Umur ${umur} Tahun`)
     }
     else if(studio == `B`){
-        alert(`Tiket ${nama}, Studio B, Umur ${umur}`)
+        alert(`Tiket ${nama}, Studio B, Umur ${umur} Tahun`)
         }
     else if(studio == `C`){
-        alert(`Tiket ${nama}, Studio C, Umur ${umur}`)
+        alert(`Tiket ${nama}, Studio C, Umur ${umur} Tahun`)
         }
 }
